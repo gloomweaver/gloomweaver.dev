2b39771dfc3cfd73e6cc0217df0950d4c0a83f22
Kirill Khramkov <hramkir@gmail.com>
2023-06-01 21:13:32 +0400

    link fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: 'Kirill Khramkov - Software Engineer - Home',
+  description: 'Portfolio site',
+};
+
 export default function Home() {
   return <main className="flex w-screen items-center justify-center">Work in progress</main>;
 }
